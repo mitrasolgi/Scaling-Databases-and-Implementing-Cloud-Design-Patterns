ee517354c28c92327cb33dd44bc9a679bfc709c3
mitrasolgi <77966797+mitrasolgi@users.noreply.github.com>
2024-06-28 06:36:44 -0400

    Update config.py

--- config.py
+++ config.py
@@ -34,12 +34,12 @@ MySQL Configuration:
     - PROXY_DB_DATABASE: Name of the proxy database.
 """
 security_group_name='FP'
-key_name='vockey'
-key_path='vockey.pem'
-region_name = 'us-east-1'
-aws_access_key_id='ASIAZKEDGPGAJDYMQSVU'
-aws_secret_access_key='8HkD5U0qZNU5I4gH08MxI4KLuCTdjgIXRy65Nh6W'
-aws_session_token='FwoGZXIvYXdzEBoaDLDOyMDxWq3xbU3PEyLHAUrRTotQUC8OMrxq7fZpqMHFX1ViSkuv45MD/BVwerAA7p2t3JU2cD67Oynwt/LU2KzPxZGZGwjOV1ulPr+Ob/qzKEn14e7FMTK8aoTcYOw5Hyvm/ESM2YmZoH30CK42qDTe1r1CZ1DH3WaraE3Z3rt4YEMMQdh5VoxNeRvDXBeiP16oT+lyq3g7WDnjvSVuIX0o4sOINO8NxGNHQIvGGRAUQXcYAnkwbfon037gwVanKVHxSJJ9Vj8EVKhRq6iTlqBUPF7kHe0o/vmrrAYyLa/KKzrivSRXtB/yPUxFRqOH5joOt1rBUKENJ7J+RsP9/vr2kTbp1UE7yPfEgg=='
+key_name=''
+key_path=''
+region_name = ''
+aws_access_key_id=''
+aws_secret_access_key=''
+aws_session_token=''
 class AWSConfig:
     MASTER_NODE_IP = "50.17.104.45"
     SLAVE_NODES_IPS = ["54.226.235.123", "34.228.70.150", "52.55.169.12"]
